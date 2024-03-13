3bc738ab5c19b03f73b5fd22072e9b11b00d48fb
Harold <cvuyante24@gmail.com>
2024-03-13 18:16:59 +0800

    add new features in project

--- src/components/Demo/ThreeDCardDemo.tsx
+++ src/components/Demo/ThreeDCardDemo.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { useAppSelector } from '@/lib/redux/store';
-
 import Image from 'next/image';
+import { TbSourceCode } from 'react-icons/tb';
+import { CgWebsite } from 'react-icons/cg';
+
+import { useAppSelector } from '@/lib/redux/store';
 import { CardBody, CardContainer, CardItem } from '../ui/3d-card';
 
 export function ThreeDCardDemo() {
@@ -11,14 +13,14 @@ export function ThreeDCardDemo() {
 		<section className='lg:mx-16 max-w-7xl grid grid-cols-1 lg:grid-cols-2 lg:gap-10'>
 			{projectList.map((item, index) => {
 				return (
-					<CardContainer key={index} className='inter-var'>
+					<CardContainer key={index} className='inter-var shadow-xl'>
 						<CardBody className='bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  '>
 							<CardItem translateZ='70' className='w-full mt-4'>
 								<Image
 									src={item.imageSrc}
 									height='1500'
 									width='1500'
-									className='h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl'
+									className='h-72 w-full object-cover rounded-xl group-hover/card:shadow-xl'
 									alt='thumbnail'
 								/>
 							</CardItem>
@@ -38,18 +40,22 @@ export function ThreeDCardDemo() {
 
 							<div className='flex justify-between items-center mt-10'>
 								<CardItem
-									translateZ={20}
+									translateZ='30'
 									as='button'
-									className='px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold'
+									className='flex items-center gap-2 px-4 py-2 rounded-xl bg-white border-2 border-goldYellow dark:bg-white dark:text-black text-black text-sm font-bold  [&>*:nth-child(odd)]:duration-200 [&>*:nth-child(odd)]:hover:translate-x-1'
 								>
-									Sign up
+									source code
+									<TbSourceCode size={16} />
 								</CardItem>
 								<CardItem
-									translateZ={20}
+									translateZ='30'
 									as='button'
-									className='px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold'
+									className='flex items-center gap-2 px-4 py-2 rounded-xl bg-goldYellow dark:bg-white dark:text-black text-white text-sm font-bold [&>*:nth-child(odd)]:duration-200 [&>*:nth-child(odd)]:hover:translate-x-1'
 								>
-									Sign up
+									live preview
+									<span>
+										<CgWebsite size={16} />
+									</span>
 								</CardItem>
 							</div>
 						</CardBody>
--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -3,7 +3,7 @@ import { libreBaskerville } from '@/lib/fonts';
 export default function TitleSection({ title }: { title: string }) {
 	return (
 		<h1
-			className={`${libreBaskerville.className} mb-4 text-goldYellow text-[3.5rem] lg:text-[4rem] font-bold uppercase text-right opacity-50`}
+			className={`${libreBaskerville.className} mb-4 text-goldYellow text-[3.5rem] lg:text-[4rem] font-bold uppercase text-right opacity-80`}
 		>
 			{title}
 		</h1>
